ab99c455d0565e520d5ec523bc82102556c371da
doniahassan111 <doniahassan077@gmail.com>
2025-06-13 19:45:49 +0300

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Homepage from "./components/homepage";
+
+function App() {
+  return (
+        <div>
+          <Homepage/>
+        </div>
+  );
+}
+
+export default App;
